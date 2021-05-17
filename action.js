5c443c5777e4bac574d88fae68466e46dd1f72ce
ajn590 <64133338+ajn590@users.noreply.github.com>
2021-05-17 20:29:08 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,19 @@
+<script src="./action.js"></script>
+
+
+
+
+function purple__btn(){
+    document.body.style.background="purple"
+   }
+   
+   
+   function green__btn()
+   {
+   document.body.style.background="red"
+   }
+   
+   function yellow__btn(){
+       document.body.style.background="green"
+   }
+   
